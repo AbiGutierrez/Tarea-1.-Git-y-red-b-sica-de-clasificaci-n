6181c9b6ea940a5e2da7eec7fb94db7567ef298b
AbiGutierrez <abigutierrez9708@gmail.com>
2022-09-09 14:48:27 -0500

    Este primer codigo es el visto en clase, sin optim

---
+++ eficiencia de la red 25AGOSTO.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 29 00:52:14 2022
+
+@author: abigu
+"""
+import mnist_loader
+import network
+import pickle
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+training_data=list(training_data)
+test_data=list(test_data)
+
+net=network.Network([784,30,10])
+
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+archivo=open("red_prueba1.pkl",'wb')
+pickle.dump(net, archivo) #dump vaciar de memoria ram a memoria de disco duro
+archivo.close()
+exit()
+
+#leer el archivo
+archivo_lectura=open("red_prueba.pkl",'rb')
+net=pickle.load(archivo_lectura)
+archivo_lectura.close()
+
+net.SGD(training_data, 10, 50, 0.5, test_data=test_data)
+
+archivo = open("red_prueba.pkl",'wb')
+pickle.dump(net,archivo)
+archivo.close()
+exit()
+
