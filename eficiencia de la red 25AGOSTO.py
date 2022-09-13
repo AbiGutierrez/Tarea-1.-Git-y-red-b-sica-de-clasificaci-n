11a14e6eb160aa2b1b6b8aa3858c6d9e4e57fd5b
AbiGutierrez <91716462+AbiGutierrez@users.noreply.github.com>
2022-09-13 00:18:49 -0500

    Update eficiencia de la red 25AGOSTO.py
    intento de implementar un optimizador y una capa softmax

--- eficiencia de la red 25AGOSTO.py
+++ eficiencia de la red 25AGOSTO.py
@@ -7,6 +7,11 @@ Created on Mon Aug 29 00:52:14 2022
 import mnist_loader
 import network
 import pickle
+#from keras.models import Sequential
+#from keras.layers import Dense
+
+import tensorflow
+
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
@@ -17,19 +22,58 @@ net=network.Network([784,30,10])
 
 net.SGD(training_data, 20, 10, 3.5, test_data=test_data)
 
+#import torchvision
+#import torch
+
+#model = build_model()
+#optimizer = torch.optim.SGD(network.Network(), lr=0.01, momentum=0.9)
+#optimizer=torch.optim.Adam(network.Network(), lr=0.001)
+
+# define la linea base del modelo
+#def baseline_model():
+  # crea el modelo
+#  model = Sequential()
+#  model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal',
+#activation='sigmoide'))
+#  model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
+  # Compila el modelo
+#  model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#  return model
+#y=torch.tensor([one-hot])
+#y_hat=torch.tensor([])
+
+#def cross_entropy(y_hat, y):
+#    return - torch.log(y_hat[range(len(y_hat)), y])
+#cross_entropy(y_hat, y)
+#loss = nn.CrossEntropyLoss(reduction='none')
+#def net(X):
+#    return softmax(torch.matmul(X.reshape((-1, w.shape[0])), w) + b)
+def softmax(y):
+    exps=np.exp(y-np.max(y))
+    return exps / np.sum(exps)
+
+def cross_entropy(x, y):
+    m=y.shape[0]
+    p=softmax(y)
+    log_likelihood=-np.log(p[range(m),y])
+    loss=np.sum(log_likelihood) / m
+    return loss
+exit()
+
 archivo=open("red_prueba1.pkl",'wb')
 pickle.dump(net, archivo) #dump vaciar de memoria ram a memoria de disco duro
 archivo.close()
 exit()
 
+
 #leer el archivo
-archivo_lectura=open("red_prueba.pkl",'rb')
+archivo_lectura=open("red_prueba1.pkl",'rb')
 net=pickle.load(archivo_lectura)
 archivo_lectura.close()
 
 net.SGD(training_data, 10, 50, 0.5, test_data=test_data)
 
-archivo = open("red_prueba.pkl",'wb')
+archivo = open("red_prueba1.pkl",'wb')
 pickle.dump(net,archivo)
 archivo.close()
 exit()
